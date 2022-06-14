2482eff05c3f1f68671d2abca5a94ca10977def6
ansel brandt <ansel@anselbrandt.com>
2022-06-14 19:42:19 -0400

    Move getBaseUrl to utils

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,21 +2,12 @@ import { withTRPC } from "@trpc/next";
 import { AppType } from "next/dist/shared/lib/utils";
 import superjson from "superjson";
 import "../styles/globals.css";
+import { getBaseUrl } from "../utils";
 import { AppRouter } from "./api/trpc/[trpc]";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
-
-function getBaseUrl() {
-  if (typeof window !== "undefined") {
-    return "";
-  }
-  if (process.browser) return ""; // Browser should use current path
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-
-  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
-}
 
 export default withTRPC<AppRouter>({
   config({ ctx }) {
