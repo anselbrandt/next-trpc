58ae1dbf39ccecdc12bb0a95dd54b071c68c1877
ansel brandt <ansel@anselbrandt.com>
2022-06-15 00:36:50 -0400

    Remove hello query

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,11 @@
 import * as trpc from "@trpc/server";
 import superjson from "superjson";
-import { z } from "zod";
 import { notesRouter } from "./notes";
 import { usersRouter } from "./users";
 
 export const appRouter = trpc
   .router()
   .transformer(superjson)
-  .query("hello", {
-    input: z
-      .object({
-        text: z.string().nullish(),
-      })
-      .nullish(),
-    resolve({ input }) {
-      return {
-        greeting: `hello ${input?.text ?? "world"}`,
-      };
-    },
-  })
   .merge("notes.", notesRouter)
   .merge("users.", usersRouter);
 
