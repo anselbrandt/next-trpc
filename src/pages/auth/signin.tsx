2c3b411e73cbdc775d22f81f5e88d4d840c8b5c8
ansel brandt <ansel@anselbrandt.com>
2022-06-16 01:27:23 -0400

    Add nextAuthLogo

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -4,21 +4,35 @@ import {
   getSession,
   signIn,
 } from "next-auth/react";
+import Image from "next/image";
+import nextAuthLogo from "../../public/logo-sm.png";
 
 const Signin = ({ csrfToken, providers }: any) => {
   if (!providers) return null;
   return (
     <div className="flex items-center justify-center h-screen">
-      {Object.values(providers).map((provider: any) => (
-        <div
-          key={provider.id}
-          className="bg-blue-500 hover:bg-blue-700 text-white text-xl py-3 px-4 rounded w-auto inline-block"
-        >
-          <button onClick={() => signIn(provider.id)}>
-            Sign in with {provider.name}
-          </button>
+      <div>
+        <div className="text-center">
+          <Image
+            src={nextAuthLogo}
+            alt="NextAuth Logo"
+            width={150}
+            height={166}
+          />
         </div>
-      ))}
+        <div>
+          {Object.values(providers).map((provider: any) => (
+            <div
+              key={provider.id}
+              className="bg-blue-500 hover:bg-blue-700 text-white text-xl py-3 px-4 rounded w-auto inline-block"
+            >
+              <button onClick={() => signIn(provider.id)}>
+                Sign in with {provider.name}
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
